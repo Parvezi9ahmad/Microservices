c7f0d04a9327b5fe33a4025266ddc5222111faf5
parvez Ahmad <parvezi9ahmad@gmail.com>
2024-01-03 08:20:55 +0530

    feign client side load balancing

--- 32_Welcome_API/src/main/java/in/ashokit/rest/WelcomeRestController.java
+++ 32_Welcome_API/src/main/java/in/ashokit/rest/WelcomeRestController.java
@@ -1,14 +1,22 @@
 package in.ashokit.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeRestController {
+	@Autowired
+	private Environment env;
+	
 
 	@GetMapping("/welcome")
 	public String getWelcomeMsg() {
-		String msg = "Welcome to Ashok It...";
+		String port = env.getProperty("server.port");
+		//To run this application click on run as configuration->select welcome API-In arguments section
+	//vm arguments type(-Dserver.port=8081 till 8083)	
+		String msg = "Welcome to Ashok It...(Server Port ::"+ port +") " ;
 		return msg;
 	}
 }
